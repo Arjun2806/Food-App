0dccf0e081110564c2d27276523a1e9b466a0214
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-07 14:40:38 +0530

    category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FoodContext } from "../context/foodContext";
 
 const Category = () => {
+
+  const {category, setCategory,handleClick} = useContext(FoodContext);
+
+  const categoryArr = Object.keys(category);
+  
   return (
     <div className="category">
       <h2>Find your favorite food</h2>
       <div className="category-buttons">
-        <button>All</button>
+        {
+          categoryArr.map((data)=>{
+              return <button key={data} style={{backgroundColor: category[data] ? '#34d399' : '#e0cccc'}} onClick={()=>handleClick(data)} >{data}</button>
+          })
+        }
+        {/* <button>All</button>
         <button>Lunch</button>
         <button>Breakfast</button>
         <button>Dinner</button>
         <button>Snacks</button>
-        <button>Drinks</button>
+        <button>Drinks</button> */}
       </div>
     </div>
   );
