bfb54541989b09e72ae654336cf07c45d3064e65
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-08 11:49:12 +0530

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+    <aside>
+      <img src="../logo.png" alt="logo" />
+      <p>
+        Yummly
+        <br />
+        Providing reliable food recipe since 1992
+      </p>
+    </aside>
+    <div className="copyright">
+    <p >© All rights reserved by Yummly.</p>
+    </div>
+  </footer>
+  
+  );
+};
+
+export default Footer;
