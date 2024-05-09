be9bbf0b809556cede00d235e4837088997ebbbf
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-09 23:53:53 +0530

    basic css is done for cart

--- src/components/CartPanel.js
+++ src/components/CartPanel.js
@@ -1,23 +1,31 @@
-import React, { useContext }  from "react";
+import React, { useContext } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { FoodContext } from "../context/foodContext";
+import { MdDelete } from "react-icons/md";
 
-const CartPanel = ({showCart}) => {
+const CartPanel = ({ showCart }) => {
+  const { cartItems } = useContext(FoodContext);
 
-   const {cartItems} = useContext(FoodContext);
-    
+  
   return (
-    <div className="cart-panel" style={{right:showCart ? "0px" : "-600px"}}>
+    <div className="cart-panel" style={{ right: showCart ? "0px" : "-600px" }}>
       <div className="cart-details">
-        <h3>Cart Details</h3>
-        <IoCloseSharp className="close-icon"  />
-        {cartItems.map((item) => (
-          <div key={item.id}>
-            <img src={item.img} alt="img" />
-            <p>{item.name}</p>
-            <p>₹{item.price}</p>
-          </div>
-        ))}
+        <div className="cart-heading">
+          <h3>Cart Details</h3>
+          <IoCloseSharp className="close-icon" />
+        </div>
+        <div className="cart-items">
+          {cartItems.map((item) => (
+            <div key={item.id} className="cart-item">
+              <img src={item.img} alt="img" />
+              <div className="cart-item-details">
+                <h4>{item.name}</h4>
+                <h5>₹{item.price}</h5>
+              </div>
+              <MdDelete className="delete-icon" />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
