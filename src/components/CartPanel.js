f3c0363bae77252849fb5509c015156f78ed0666
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-10 13:34:11 +0530

    total is done

--- src/components/CartPanel.js
+++ src/components/CartPanel.js
@@ -2,11 +2,12 @@ import React, { useContext } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { FoodContext } from "../context/foodContext";
 import { MdDelete } from "react-icons/md";
+import { FiMinusCircle } from "react-icons/fi";
+import { FiPlusCircle } from "react-icons/fi";
 
 const CartPanel = ({ showCart }) => {
   const { cartItems } = useContext(FoodContext);
 
-  
   return (
     <div className="cart-panel" style={{ right: showCart ? "0px" : "-600px" }}>
       <div className="cart-details">
@@ -21,10 +22,27 @@ const CartPanel = ({ showCart }) => {
               <div className="cart-item-details">
                 <h4>{item.name}</h4>
                 <h5>₹{item.price}</h5>
+                <div className="quantity">
+                  <FiMinusCircle className="minus-icon" />
+                  <span>1</span>
+                  <FiPlusCircle className="plus-icon" />
+                </div>
               </div>
               <MdDelete className="delete-icon" />
             </div>
           ))}
+        </div>
+        <div class="checkout-summary">
+          <h3 >
+            Items: <span >1</span>
+          </h3>
+          <h3 >
+            Total Amount: <span >₹100</span>
+          </h3>
+          <hr />
+          <button >
+            Checkout
+          </button>
         </div>
       </div>
     </div>
