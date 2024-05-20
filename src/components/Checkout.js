8ed95946112c59c03e4876a3ad73e13fbeb457a8
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-20 15:24:30 +0530

    checkout page updated

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -8,38 +8,73 @@ const Checkout = () => {
           <h1>Checkout</h1>
         </div>
         <div className="checkout-container">
+          {/* DELIVERY SECTION STARTS FROM HERE */}
           <div className="left-section">
             <div className="delivery-section">
               <div className="delivery-heading">
                 <h3>1. DELIVERY OPTIONS</h3>
               </div>
               <div className="address-section">
-                <input type="text" className="name" placeholder=" Enter Full Name" />
+                <input
+                  type="text"
+                  className="name"
+                  placeholder=" Enter Full Name"
+                />
                 <div className="Mobile-number">
-                  <input type="number" className="number" placeholder="Mobile Number" />
-                  <input type="text" className="email" placeholder="Email Address" />
+                  <input
+                    type="number"
+                    className="number"
+                    placeholder="Mobile Number"
+                  />
+                  <input
+                    type="text"
+                    className="email"
+                    placeholder="Email Address"
+                  />
                 </div>
-                <textarea id="address" className="address" name="address" rows="9" cols="60" placeholder="Enter Your Address"></textarea>
+                <textarea
+                  id="address"
+                  className="address"
+                  name="address"
+                  rows="9"
+                  cols="60"
+                  placeholder="Enter Your Address"
+                ></textarea>
               </div>
               <button className="save-button">SAVE & CONTINUE</button>
             </div>
+
+            {/* PAYMENT SECTION STARTS FROM HERE */}
+
             <div className="payment-section">
               <h3>2. PAYMENT OPTIONS</h3>
-           </div>
+            </div>
+
+            {/* ORDER REVIEW SECTION STARTS FROM HERE */}
+
             <div className="order-review">
-            <h3>3. ORDER REVIEW</h3>  </div>
+              <h3>3. ORDER REVIEW</h3>{" "}
+            </div>
           </div>
+
+          {/* CART SECTION STARTS FROM HERE */}
+
           <div className="cartpage">
-            <div className="cart-heading">
+            <div className="cart-heading1">
               <h3>IN YOUR CART</h3>
             </div>
             <div className="cart-total">
-                <h5>Total Amount: ₹500</h5>
-                <h5>Estimated Delivery: ₹110</h5>
-                <h4>Total: <span>₹610</span></h4>
+              <p>Total Amount <span1>₹500</span1> </p>
+              <p>Estimated Delivery <span2>₹110</span2> </p>
+              <h4>
+                TOTAL <span>₹610</span>
+              </h4>
             </div>
-            <hr/>
+            <hr />
             <div className="cart-items">
+              <div className="cart-heading2">
+                <h4>ARRIVES BY THU, 22 JUL</h4>
+              </div>
               <div className="cart-image">
                 <img src="" alt="pizza" />
               </div>
@@ -57,30 +92,3 @@ const Checkout = () => {
 };
 
 export default Checkout;
-
-//.flex-container {
-//   display: flex;
-//   flex-direction: row;
-//   flex-wrap: nowrap;
-//   justify-content: center;
-//   align-items: center;
-//   align-content: normal;
-// }
-
-// .flex-items:nth-child(1) {
-//   display: block;
-//   flex-grow: 0;
-//   flex-shrink: 1;
-//   flex-basis: auto;
-//   align-self: auto;
-//   order: 0;
-// }
-
-// .flex-items:nth-child(2) {
-//   display: block;
-//   flex-grow: 0;
-//   flex-shrink: 1;
-//   flex-basis: auto;
-//   align-self: auto;
-//   order: 0;
-// }
