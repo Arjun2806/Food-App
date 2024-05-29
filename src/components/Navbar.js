be48023ec6646398a1ea2f2f08d40d218075bfd5
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-29 11:31:49 +0530

    dark theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,29 @@
 import { useContext } from "react";
 import { FoodContext } from "../context/FoodContext";
 import { TiShoppingCart } from "react-icons/ti";
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { useColorScheme } from "../hooks/useColorScheme";
 
 const Navbar = ({ toggle }) => {
   const { handleSearch, input } = useContext(FoodContext);
+
+  const { isDark, setIsDark } = useColorScheme();
+
 
   return (
     <div className="navbar">
       <img src="./logo.png" className="App-logo" alt="logo" />
       <h1>Yummly</h1>
-      {/* <div className="cart" onClick={toggle}> */}
       <TiShoppingCart className="cart-icon" onClick={toggle} />
-      {/* </div> */}
+
+      <DarkModeSwitch
+        onChange={setIsDark}
+        checked={isDark}
+        className="dark-mode-switch"
+        animationProperties={{ duration: 300 }}
+        aria-label="Dark mode toggle"
+      />
+
       <input
         type="search"
         name="search"
