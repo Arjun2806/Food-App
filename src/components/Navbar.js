b2ea431a05941b9539984fdd75372dd521b22730
Arjun Suthar <arjunsuthar3427@gmail.com>
2024-05-31 21:01:23 +0530

    about page added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { FoodContext } from "../context/FoodContext";
 import { TiShoppingCart } from "react-icons/ti";
-import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useMediaQuery } from "react-responsive";
-
+import { Link } from "react-router-dom";
 
 // set localstorage and read the values from localstorage
-const useColorScheme =() => {
+const useColorScheme = () => {
   const systemPrefersDark = useMediaQuery({
     query: "(prefers-color-scheme: dark)",
   });
@@ -29,7 +29,7 @@ const useColorScheme =() => {
   }, [isDark]);
 
   useEffect(() => {
-    console.log(value);
+    // console.log(value);
     if (value) {
       document.body.classList.add("dark");
     } else {
@@ -41,8 +41,7 @@ const useColorScheme =() => {
     isDark: value,
     setIsDark,
   };
-}
-
+};
 
 const Navbar = ({ toggle }) => {
   const { handleSearch, input } = useContext(FoodContext);
@@ -51,47 +50,47 @@ const Navbar = ({ toggle }) => {
   const [isFocused, setIsFocused] = useState(false);
   const searchInputRef = useRef(null);
 
-const handleonClick = () => {
-  setIsFocused(true);
-  searchInputRef.current.focus();
-};
-
+  const handleonClick = () => {
+    setIsFocused(true);
+    searchInputRef.current.focus();
+  };
 
   return (
     <div className="navbar">
       <div className="logo-container">
-      <img src="./logo.png" className="App-logo" alt="logo" />
+        <Link to="/about">
+          <img src="./logo.png" className="App-logo" alt="logo" />
+        </Link>
       </div>
       <h1>Yummly</h1>
       <div className="search-container">
-      <TiShoppingCart className="cart-icon" onClick={toggle} />
-      <div className="theme-container">
-      <p>Switch Theme</p>
-      <DarkModeSwitch
-        onChange={setIsDark}
-        checked={isDark}
-        className="dark-mode-switch"
-        animationProperties={{ duration: 400 }}
-        aria-label="Dark mode toggle"
-        // sunColor="#FFA500"
-        sunColor="#FF7F50"
-      />
+        <TiShoppingCart className="cart-icon" onClick={toggle} />
+        <div className="theme-container">
+          <p>Switch Theme</p>
+          <DarkModeSwitch
+            onChange={setIsDark}
+            checked={isDark}
+            className="dark-mode-switch"
+            animationProperties={{ duration: 400 }}
+            aria-label="Dark mode toggle"
+            // sunColor="#FFA500"
+            sunColor="#FF7F50"
+          />
+        </div>
+        <div className="search-bar-container">
+          <FaMagnifyingGlass className="search-icon" onClick={handleonClick} />
+          <input
+            type="search"
+            name="search"
+            placeholder="Search here"
+            value={input}
+            onChange={handleSearch}
+            ref={searchInputRef}
+            className={isFocused ? "focused" : ""}
+            onBlur={() => setIsFocused(false)}
+          />
+        </div>
       </div>
-      <div className="search-bar-container">
-      <FaMagnifyingGlass className="search-icon" onClick={handleonClick} />
-       <input
-        type="search"
-        name="search"
-        placeholder="Search here"
-        value={input}
-        onChange={handleSearch}
-        ref={searchInputRef}
-        className={isFocused ? "focused" : ""}
-        onBlur={() => setIsFocused(false)}
-      />
-      </div>
-      </div>
-   
     </div>
   );
 };
